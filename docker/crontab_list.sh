eb6f6e17a91a724a5710fc2dd9d4e5b625edfda6
Takicheng <42823161+Takicheng@users.noreply.github.com>
2021-06-30 08:36:34 +0800

    fix

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -174,5 +174,3 @@
 #1 0-23/2 * * * node /scripts/jd_qqxing.js >> /scripts/logs/jd_qqxing.log 2>&1
 #欧洲狂欢杯
 55 59 9  * * node /scripts/jd_europeancup.js >> /scripts/logs/jd_europeancup.log 2>&1
-#py宠汪汪兑换Pro
-59 7,15,23 * * * * py /scripts/jd_joy_reward_new.py
